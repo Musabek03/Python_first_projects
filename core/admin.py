3fa3f9079e5062505a99a5c6fe510a6c983099b5
Musabek <makhambetjaliev@gmail.com>
2025-09-26 11:29:25 +0500

    Admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
+
+
+#admin.site.register(Post)
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    list_filter = ('title',)
+    search_fields = ('title', 'content')
 
 # Register your models here.
+
